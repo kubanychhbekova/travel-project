25db61e84586818f25872f2e885fd1caa12364cb
kubanychhbekova <a7966519@gmail.com>
2023-08-08 18:52:18 +0600

    finish header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.scss';
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import {Route, Routes} from "react-router-dom";
+import IssytKyl from "./components/regions/issytKyl";
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Routes>
+                <Route path={"/issykKul"} element={<IssytKyl/>}/>
+            </Routes>
             <Footer/>
         </div>
     );
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,9 +2,49 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer>
+           <div className="container">
+               <div className="footer">
+                   <div className="footer--regions">
+                       <h1>logo</h1>
+                   </div>
+                   <div className="footer--regions">
+                       <h1>Home</h1>
+                       <div className="footer--regions__title">
+                           <p>Attractions</p>
+                           <p>Map</p>
+
+                       </div>
+                   </div>
+                   <div className="footer--regions">
+                       <h1>Regions</h1>
+                       <div className="footer--regions__title">
+                           <p>Batken</p>
+                           <p>Jalal-Abad</p>
+                           <p>Issyk_kul</p>
+                           <p>Naryn</p>
+                           <p>Osh</p>
+                           <p>Talas</p>
+                           <p>Chyi</p>
+                       </div>
+                   </div>
+                   <div className="footer--regions">
+                       <h1>Culture</h1>
+                       <div className="footer--regions__title">
+                           <p>Games</p>
+                           <p>National instruments</p>
+                           <p>National clothes</p>
+                           <p>Hand crafts</p>
+                           <p>Currency</p>
+                           <p>Kitchen</p>
+                       </div>
+                   </div>
+                   <div className="footer--regions">
+                       <h1>Gallery</h1>
+                   </div>
+               </div>
+           </div>
+        </footer>
     );
 };
 
